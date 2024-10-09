b203edd530ea951c98b73ff62affd411203ee826
Sky King <skyking@epiccash.com>
2024-10-09 15:38:22 -0400

    Update ndsmtp.php

--- ndsmtp.php
+++ ndsmtp.php
@@ -2,23 +2,20 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
-//test
-require '/mnt/ssd480/NowDeals/PHPMailer.php';
-require '/mnt/ssd480/NowDeals/SMTP.php';
-require '/mnt/ssd480/NowDeals/Exception.php';
+
 //prod
-//require '/var/www/html/members/PHPMailer.php';
-//require '/var/www/html/members/SMTP.php';
-//require '/var/www/html/members/Exception.php';
+require '/var/www/html/members/PHPMailer.php';
+require '/var/www/html/members/SMTP.php';
+require '/var/www/html/members/Exception.php';
 
 $mail = new PHPMailer(true);
 
 //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
 $mail->isSMTP();
-$mail->Host       = 'mail.nowdeals.com';
+$mail->Host       = 'your.mailserver';
 $mail->SMTPAuth   = true;
 $mail->Username   = 'mailer@nowdeals.com';
-$mail->Password   = 'R;vBY5BSgz';
+$mail->Password   = 'password';
 //$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
 //$mail->Port       = 465;
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
